5eb99a3f91660d5f3bc4a6ad1196af7bab156c46
Akash Sarode <akashtechnion@gmail.com>
2026-01-25 13:30:24 -0500

    Created first announcement

--- src/lib/config/announcements.js
+++ src/lib/config/announcements.js
@@ -1,14 +1,7 @@
 export const announcements = [
   {
     id: "welcome-v1",
-    text: "Welcome to Cipher Arena! Check out our [Discord](https://discord.gg/cipherarena) for updates.",
-    active: true,
-    dismissible: true,
-    type: "info", // info, warning, success
-  },
-  {
-    id: "welcome-v2",
-    text: "Hello",
+    text: "Welcome to Cipher Arena! Check out our [Discord](https://discord.gg/ybhdyEnjZP) for updates.",
     active: true,
     dismissible: true,
     type: "info", // info, warning, success
